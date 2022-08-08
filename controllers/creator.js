52ce9cf66e0e7426567a46a829f566d0b446b965
AntonKolomiiets <anton.kolomiiets@logz.io>
2022-08-08 12:30:29 +0300

    add refactor round 1

--- controllers/creator.js
+++ controllers/creator.js
@@ -7,42 +7,6 @@ const { createLambda } = require('../utils/lambda-creator');
 const { cloudWatchEvent } = require('../utils/cloudWatchEvent');
 const { NAME_OF_ZIP_FILE } = require('../utils/constants');
 
-exports.creator = async (req, res, next) => {
-    console.log('1');
-    const { name, description, code } = req.body;
-    try {
-        // const fileRoute = path.join(__dirname, '..', NAME_OF_ZIP_FILE);
-        // const data = await updateFile(code)
-        //     .then((status) => status)
-        //     .catch((err) => console.log(err));
-        // console.log(data);
-        // const resp = await fileToZip()
-        //     .then((result) => {
-        //         console.log(result);
-        //     })
-        //     .then((res) => {})
-        //     .then((resp) => {})
-        //     .catch((err) => {
-        //         console.log(err);
-        //     });
-        // const readData = fs.readFileSync(fileRoute, 'utf8');
-        // let result;
-        // if (readData) {
-        //     result = await uploadFileOnS3(NAME_OF_ZIP_FILE, readData);
-        // } else {
-        //     throw Error('Failed to upload data');
-        // }
-        // console.log(result);
-        const lambdaResp = await createLambda(name, description);
-        console.log(lambdaResp);
-        res.statusCode = 200;
-        res.send({ message: 'sucessfull' });
-    } catch (err) {
-        console.log(err);
-        res.status(404).send({ error: true, err });
-    }
-};
-
 exports.modifyFile = async (req, res, next) => {
     const { code } = req.body;
 
