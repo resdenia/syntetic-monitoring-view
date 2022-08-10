631bc6f44638bd02eeac30230ee5d09cbd5ba4ec
AntonKolomiiets <anton.kolomiiets@logz.io>
2022-08-10 12:35:46 +0300

    loop in har logs

--- service/lambdaFunctionLocal/index.js
+++ service/lambdaFunctionLocal/index.js
@@ -1,23 +1,34 @@
 require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
+const convertHarToJSON = require('./convertHarToJSON');
 
 const playwright = require('playwright-aws-lambda');
 const { PlaywrightHar } = require('playwright-har');
-
-
+console.log(process.env.TOKEN);
+const token = process.env.TOKEN;
+const logger = require('logzio-nodejs').createLogger({
+    token: token,
+    protocol: 'https',
+    host: 'listener.logz.io',
+    port: '8071',
+    debug: true,
+    bufferSize: 50,
+    type: 'syntetic-scripting', // OPTIONAL (If none is set, it will be 'nodejs')
+    sendIntervalMs: 1000,
+});
 const handler = async (event, context) => {
-	let browser = null;
-	let harData = null;
-	try {
-		browser = await playwright.launchChromium();
-		const context = await browser.newContext();
+    let browser = null;
+    let harData = null;
+    try {
+        browser = await playwright.launchChromium();
+        const context = await browser.newContext();
 
-		const page = await context.newPage();
-		const playwrightHar = new PlaywrightHar(page);
-		await playwrightHar.start();
+        const page = await context.newPage();
+        const playwrightHar = new PlaywrightHar(page);
+        await playwrightHar.start();
 
-  await page.goto('https://logz.io/');
+        await page.goto('https://logz.io/');
 
         await page.setViewportSize({ width: 1850, height: 877 });
 
@@ -32,15 +43,24 @@ const handler = async (event, context) => {
         await page.click(
             '.navigation-body > .navigation-body-section_ > .navigation-menu > .navigation-item:nth-child(2) > .navigation-link',
         );
-      harData = await playwrightHar.stop('./public/output/local.har');
-} catch (error) {
-throw error;
-} finally {
-if (browser) {
-	await browser.close();
-}
-}
+        harData = await playwrightHar.stop();
+    } catch (error) {
+        throw error;
+    } finally {
+        if (browser) {
+            await browser.close();
+        }
+    }
+    const parsedData = convertHarToJSON(harData);
+    console.log(parsedData.result.length);
 
-return true;
+    // console.log(parsedData.flatEntries[1]);
+
+    parsedData.result.forEach((log) => {
+        // console.log(log);
+        logger.log(log);
+    });
+
+    return true;
 };
 handler();
